0f8570d0512267b85bd2a96b5402885431b68627
thechinh98 <chinh1998@gmail.com>
2019-08-09 05:48:23 +0700

    Initial commit

---
+++ app/src/main/java/com/example/schoolapp/ActivitySignin.kt
@@ -0,0 +1,12 @@
+package com.example.schoolapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySignin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
---
+++ app/src/main/java/com/example/schoolapp/ActivitySignup.kt
@@ -0,0 +1,12 @@
+package com.example.schoolapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySignup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
